8a39acd63a1c9df114759a6cf061a502cd47f0e7
¨Viachaslau_Mukhin¨ <viachaslau.mukhin@expert-soft.by>
2023-08-07 20:13:23 +0300

    Add new files

---
+++ src/lesson2/Main.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0, j = 0; i < 100; i++, j++)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/lesson2/hw2/Main.java
@@ -0,0 +1,5 @@
+package lesson2.hw2;
+
+public class Main
+{
+}
